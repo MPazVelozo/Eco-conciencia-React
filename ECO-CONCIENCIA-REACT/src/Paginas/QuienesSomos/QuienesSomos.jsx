b00900fc4c478c58bf06a0a68220c1f63c234066
Uwumila <camila.fuentes@genstudents.org>
2024-05-28 17:41:18 -0400

    a

---
+++ ECO-CONCIENCIA-REACT/src/Paginas/QuienesSomos/QuienesSomos.jsx
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="es">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Equipo de Sostenibilidad</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            background-image: url('../images/v223_7.png');
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-position: center;
+        }
+
+        .card {
+            background-color: rgba(255, 255, 255, 0.8); 
+        }
+
+        .navbar, .footer, .card-body {
+            background-color: rgba(255, 255, 255, 0.8); 
+        }
+    </style>
+    <link href="stylequienes.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+    <!-- botones -->
+    <button type="button" class="btn btn-secondary custom-button">INICIO</button>
+    <!-- texto -->
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h1 class="display-4" id="titulo">¿Quiénes somos?</h1>
+                <p class="lead">Somos un equipo comprometido con el medio ambiente y creemos en la importancia de educar
+                    a los niños sobre la sostenibilidad. Nos proponemos desarrollar una página web interactiva que sirva
+                    como herramienta educativa, con el objetivo de fomentar en los niños el cuidado y la protección del
+                    medio ambiente.</p>
+                <p>A través de esta plataforma, buscamos proporcionar recursos educativos y actividades interactivas
+                    diseñadas para inspirar y motivar a los niños a adoptar prácticas responsables que promuevan la
+                    conservación de nuestro planeta.</p>
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-md-3 text-center">
+                <div class="card">
+                    <img src="https://i.pinimg.com/736x/53/04/c5/5304c5d8625cfb2f96e9e31516631c2e.jpg" class="card-img-top" alt="foto">
+                    <div class="card-body">
+                        <h5 class="card-title">Alejandra Camacaro</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 text-center">
+                <div class="card">
+                    <img src="https://i.pinimg.com/736x/6c/29/64/6c2964d1d8d496abf8eeb47921ac5b2c.jpg" class="card-img-top" alt="foto">
+                    <div class="card-body">
+                        <h5 class="card-title">María Paz Velozo</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 text-center">
+                <div class="card">
+                    <img src="https://i.pinimg.com/736x/78/bf/e1/78bfe15b2de15a44b890a10589639b6b.jpg" class="card-img-top" alt="foto">
+                    <div class="card-body">
+                        <h5 class="card-title">Camila Fuentes</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 text-center">
+                <div class="card">
+                    <img src="https://i.pinimg.com/736x/37/43/01/374301145e6cbdee10055ec09c63b05e.jpg" class="card-img-top" alt="foto">
+                    <div class="card-body">
+                        <h5 class="card-title">Cynthia Arenas</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+   <!-- footer --> 
+   <!----<footer class="bg-light text-center text-lg-start mt-5">
+        <div class="container p-4">
+            <p class="text-center">&copy; 2024 Eco-conciencia. Todos los derechos reservados.</p>
+        </div>
+    </footer>---->
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="scriptquienes.js"></script> >
+</body>
