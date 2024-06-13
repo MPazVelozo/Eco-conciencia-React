58dfa7f77c0a804fc89344541d161a0dac18815a
MPazVelozo <165829671+MPazVelozo@users.noreply.github.com>
2024-06-13 11:28:10 -0400

    arreglos

--- ECO-CONCIENCIA-REACT/src/Componentes/Qhacemos.jsx
+++ ECO-CONCIENCIA-REACT/src/Componentes/Qhacemos.jsx
@@ -5,12 +5,12 @@ import './Qhacemos.css'
 function Qhacemos() {
   return (
     <div>
-        <h1>Qué hacemos?</h1>
-    <h2>Somos un grupo de estudiantes que actualmente participan en un
+        <h3>Qué hacemos?</h3>
+    <h4>Somos un grupo de estudiantes que actualmente participan en un
     bootcamp de desarrolladoras full Stack JavaScript, la cual estamos
     comprometidas con la práctica de habilidades y conocimientos adquiridos
     en este curso. Además reconocemos la importancia de la educación
-    ambiental y la sensibilización desde una edad temprana</h2>
+    ambiental y la sensibilización desde una edad temprana</h4>
     </div>
   )
 }
