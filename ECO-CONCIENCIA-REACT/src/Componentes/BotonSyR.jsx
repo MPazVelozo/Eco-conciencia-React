af0c577c75e55a9216c9bbdd3cd937168894c5d6
alejandra <alejandracamacaro2@gmail.com>
2024-06-26 09:36:54 -0400

    cambios

--- ECO-CONCIENCIA-REACT/src/Componentes/BotonSyR.jsx
+++ ECO-CONCIENCIA-REACT/src/Componentes/BotonSyR.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import './BotonSyR.css'
 
 function BotonSyR() {
   return (
-    <Link to="/Sugerencias" className="btn btn-secondary new-custom-button">Sugerencias y Reclamos</Link>
+    <Link to="/Sugerencias" className="btn btn-primary" disabled data-bs-toggle>Sugerencias y reclamos
+    </Link>
     // <button type="button" className="btn btn-secondary custom-buttom">INICIO</button>
   )
 }
